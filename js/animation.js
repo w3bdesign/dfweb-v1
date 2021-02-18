6f2d863d1a462b57ee532000fd9626c2bfb270f7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-02-18 07:02:02 +0100

    Replace for loop with forEach

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,29 @@
+/** global: ScrollMagic */
 const startScrollMagic = () => {
   const controller = new ScrollMagic.Controller();
-  const revealElements = document.getElementsByClassName("reveal");
-  for (let i = 0; i < revealElements.length; i++) {
+  const revealElements = document.getElementsByClassName('reveal');
+  const revealElementCount = revealElements.length - 1;
+  const elementsArray = Array.from(revealElements);
+
+  let counter = 0;
+
+  // Replaced for loop here with modern ES6 syntax and forEach
+  elementsArray.forEach((_) => {
+    const counterEqElementCount = counter === revealElementCount;
+    const myOffset = counterEqElementCount ? -100 : 200;
+    new ScrollMagic.Scene({
+      triggerElement: revealElements[counter],
+      offset: myOffset, // standard = 100
+      reverse: false,
+      triggerHook: 0.9,
+    })
+      .setClassToggle(revealElements[counter], 'visible')
+      .addTo(controller);
+    counter++;
+  });
+};
+
+/*for (let i = 0; i < revealElements.length; i++) {
     const myOffset = i === revealElements.length - 1 ? -100 : 200;
     new ScrollMagic.Scene({
       triggerElement: revealElements[i],
@@ -12,6 +34,6 @@ const startScrollMagic = () => {
       .setClassToggle(revealElements[i], "visible")
       .addTo(controller);
   }
-};
+};*/
 
 startScrollMagic();
